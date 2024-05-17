48508159a3050f7d7f65ec17415023fe475d39bd
suheri77 <153284084+suheri77@users.noreply.github.com>
2024-05-17 15:04:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ const questions = [
 let currentQuestionIndex = 0;
 let userScore = 0;
 let restartAttempts = 0;
-const maxRestarts = 3;
+const maxRestarts = 2;
 
 const startButtonEl = document.querySelector(".start-btn");
 const welcomeScreenEl = document.querySelector(".welcome-screen");
