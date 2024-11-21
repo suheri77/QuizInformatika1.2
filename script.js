50dc7d90f4be35d94bf3b8253f50ffa4daa1e172
suheri77 <153284084+suheri77@users.noreply.github.com>
2024-11-22 06:32:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,179 +1,120 @@
-//Version 2//
+//Original Version//
 const questions = [
+
+
     {
-        question: "Perkembangan komputer pribadi seperti IBM PC pada tahun 1980-an memberikan akses kepada masyarakat untuk:", //1//
+        question: "Translate *Angry", //1//
         answers: [
-            { text: "Terhubung dengan antariksa", correct: false },
-            { text: "Menjalankan bisnis besar", correct: false },
-            { text: "Mengakses informasi dan komunikasi pribadi", correct: true },
-            { text: "Menjadi lebih terisolasi secara sosial", correct: false },
-            { text: "Mempertahankan gaya hidup tradisional", correct: false },
+            { text: "Lapar", correct: false },
+            { text: "Senang", correct: false },
+            { text: "Marah", correct: true },
+            { text: "Ramah", correct: false },
+            { text: "Sopan", correct: false },
         ],
     },
 
     {
-        question: "Peran internet dalam komunikasi sosial ditandai oleh:", //2//
+        question: "Translate *Hungry*", //2//
         answers: [
-            { text: " Munculnya surat kabar cetak", correct: false },
-            { text: "Pertumbuhan media sosial dan jejaring online", correct: true },
-            { text: "Penurunan minat dalam budaya populer", correct: false },
-            { text: "Penurunan penggunaan perangkat lunak komputer", correct: false },
-            { text: "Peningkatan dalam perdagangan luar negeri", correct: false },
+            { text: "Malas", correct: false },
+            { text: "Senang", correct: false },
+            { text: "Sakit", correct: false },
+            { text: "Lapar", correct: true },
+            { text: "Memperkuat", correct: false },
         ],
     },
     {
-        question: "Bagaimana perkembangan komputer telah mengubah cara pendidikan disampaikan dan diterima di masyarakat?", //3//
+        question: "Translate *Tired*", //3//
         answers: [
-            { text: "Mendorong pembelajaran berbasis teknologi", correct: true },
-            { text: "Mengurangi akses ke informasi", correct: false },
-            { text: "Meningkatkan ketidaksetaraan pendidikan", correct: false },
-            { text: "Menghambat kemampuan siswa untuk berkolaborasi", correct: false },
-            { text: "Meningkatkan kecenderungan plagiarisme", correct: false },
-        ],
-    },
-
-    {
-        question: "Apa dampak positif dari peningkatan aksesibilitas internet di masyarakat?",  //4//
-        answers: [
-            { text: "Peningkatan kolaborasi global", correct: true },
-            { text: "Peningkatan isolasi sosial", correct: false },
-            { text: "Penurunan keterampilan teknologi", correct: false },
-            { text: "Perubahan signifikan dalam kebiasaan makan", correct: false },
-            { text: "Penurunan minat dalam berolahraga", correct: false },
+            { text: "Bahagia", correct: false},
+            { text: "Lelah", correct: true },
+            { text: "Bersemangat", correct: false },
+            { text: "Bingung", correct: false },
+            { text: "Sedih", correct: false },
         ],
     },
 
     {
-        question: "Bagaimana komputer telah mempengaruhi industri hiburan dan budaya populer?", //5//
+        question: "Translate *Comfortable*", //4//
         answers: [
-            { text: "Mengurangi variasi dalam bentuk hiburan", correct: false },
-            { text: "Meningkatkan partisipasi dalam olahraga", correct: false },
-            { text: "Memberdayakan seniman untuk bereksperimen", correct: true },
-            { text: "Mempercepat hilangnya bahasa dan budaya lokal", correct: false },
-            { text: "Meningkatkan minat dalam pertunjukan langsung", correct: false },
+            { text: "Nyaman", correct: true },
+            { text: "Marah", correct: false },
+            { text: "Lapar", correct: false },
+            { text: "Sedih", correct: false },
+            { text: "Panas", correct: false },
         ],
     },
 
     {
-
-        question: "Siapakah ilmuwan yang dikenal karena perannya dalam memecahkan kode Enigma Jerman selama Perang Dunia II dan kontribusinya dalam pengembangan komputer modern yang digunakan banyak orang-orang saat ini?", //6//
+        question: "Translate *Uncomfortable*", //5//
         answers: [
-            { text: "Charles Babbage", correct: false },
-            { text: "John von Neumann", correct: false },
-            { text: "Grace Hopper", correct: false },
-            { text: " Joseph Oppenheimer", correct: false },
-            { text: "Alan Turing", correct: true },
+            { text: " Nyaman", correct: false },
+            { text: "Takut", correct: false },
+            { text: "Tidak nyaman", correct: true },
+            { text: "Sakit", correct: false },
+            { text: "Cantik", correct: false },
         ],
     },
 
     {
-
-        question: "Manakah dari berikut ini yang bukan dampak positif perkembangan komputer terhadap kehidupan sosial?", //7//
+        question: "Translate *Young*", //6//
         answers: [
-            { text: "Meningkatkan akses informasi dan komunikasi", correct: false },
-            { text: "Mempermudah transaksi keuangan dan perdagangan ", correct: false },
-            { text: "Mempercepat proses belajar dan mengajar ", correct: false },
-            { text: "Meningkatkan keterpencilan dan isolasi sosial ", correct: true },
-            { text: "Membuka peluang kerja baru di bidang teknologi", correct: false },
+            { text: "Muda", correct: true },
+            { text: "Tua", correct: false },
+            { text: "Besar", correct: false },
+            { text: "Kecil", correct: false },
+            { text: "Tinggi", correct: false },
+            
         ],
     },
 
     {
-        question: "Bagaimana perkembangan komputer memengaruhi cara kita berinteraksi satu sama lain?", //8//
+        question: "Translate *Old*", //7//
         answers: [
-            { text: "Mengurangi interaksi tatap muka dan meningkatkan interaksi online ", correct: false },
-            { text: "Mempermudah komunikasi jarak jauh dan memperkuat hubungan antar individu ", correct: false },
-            { text: "Memicu cyberbullying dan pelecehan online ", correct: false },
-            { text: "Semua jawaban benar ", correct: false },
-            { text: " Hanya jawaban A dan B yang benar", correct: true },
-        ],
-    },
-
-   
-
-    {
-        question: " Bagaimana perkembangan komputer memengaruhi cara kita mengonsumsi informasi?", //9//
-        answers: [
-            { text: "Meningkatkan akses ke berbagai sumber informasi ", correct: false },
-            { text: " Memudahkan verifikasi kebenaran informasi ", correct: false },
-            { text: "Meningkatkan risiko penyebaran informasi palsu ", correct: false },
-            { text: "Semua jawaban benar ", correct: true },
-            { text: "Hanya jawaban A dan B yang benar", correct: false },
-        ],
-    },
-
-
-    {
-        question: "Bagaimana perkembangan komputer memengaruhi cara masyarakat berbelanja?", //10//
-        answers: [
-            { text: "Mempermudah perbandingan harga dan produk ", correct: false },
-            { text: "Semua jawaban benar ", correct: true },
-            { text: "Munculnya e-commerce dan platform belanja online ", correct: false },
-            { text: " Meningkatkan risiko penipuan online ", correct: false },
-            { text: "Hanya jawaban A dan B yang benar", correct: false },
+            { text: "Cepat", correct: false },
+            { text: "Baru", correct: false },
+            { text: "Tua", correct: true },
+            { text: "Muda", correct: false },
+            { text: "Sulit", correct: false },
+           
         ],
     },
 
     {
-
-        question: ". Apa yang dimaksud dengan hak kekayaan intelektual (HKI) dalam konteks produk informatika?", //11//
+        question: "Translate *Fresh*", //8//
         answers: [
-            { text: "Hak untuk mengakses internet gratis", correct: false },
-            { text: "Hak untuk mengekspor perangkat lunak", correct: false },
-            { text: "Hak untuk mengklaim kepemilikan atas karya kreatif", correct: true },
-            { text: "Hak untuk menghapus data pribadi dari internet", correct: false },
-            { text: " Hak untuk menyalin dan mendistribusikan konten tanpa izin", correct: false },
+            { text: "Panas", correct: false },
+            { text: "Segar", correct: true },
+            { text: "Kotor", correct: false },
+            { text: "Sulit", correct: false },
+            { text: "Sakit", correct: false },
+            
         ],
     },
 
     {
-
-        question: "Bagaimana produk informatika berkontribusi terhadap pertumbuhan ekonomi?", //12//
+        question: "Translate *Rotten*", //9//
         answers: [
-            { text: "Dengan menciptakan lapangan kerja baru", correct: true },
-            { text: "Dengan membatasi akses informasi", correct: false },
-            { text: "Dengan meningkatkan biaya produksi", correct: false },
-            { text: " Dengan mengurangi investasi dalam teknologi ", correct: false },
-            { text: "Dengan meningkatkan kebutuhan akan bahan bakar fosil", correct: false },
+            { text: "Sulit", correct: false },
+            { text: "Keras", correct: false },
+            { text: "Busuk", correct: true },
+            { text: "Baik", correct: false },
+            { text: "Segar", correct: false },
+        
         ],
     },
 
     {
-        question: "Apa yang dimaksud dengan perangkat lunak sumber terbuka (open source software) dalam konteks ekonomi produk informatika?", //13//
+        question: "Translate *Full*", //10//
         answers: [
-            { text: "Perangkat lunak yang hanya bisa diakses oleh pembeli berlangganan", correct: false },
-            { text: "Perangkat lunak yang hanya tersedia untuk perusahaan besar", correct: false },
-            { text: "Perangkat lunak yang tidak dapat diakses secara online", correct: false },
-            { text: "Perangkat lunak yang hanya bisa diinstal pada satu perangkat saja", correct: false },
-            { text: " Perangkat lunak yang dapat dimodifikasi dan didistribusikan secara bebas", correct: true },
+            { text: "Kosong", correct: false },
+            { text: "Sempit", correct: false },
+            { text: "Penuh", correct: true },
+            { text: "Kecil", correct: false },
+            { text: "Lemah", correct: false },
+            
         ],
     },
-
-   
-
-    {
-        question: "Mengapa perlindungan data pribadi merupakan isu penting dalam hukum produk informatika?", //14//
-        answers: [
-            { text: "Untuk membatasi akses pengguna internet", correct: false },
-            { text: " Untuk mempromosikan pelanggaran privasi ", correct: false },
-            { text: "Untuk meningkatkan risiko keamanan siber", correct: false },
-            { text: "Untuk melindungi informasi pribadi pengguna ", correct: true },
-            { text: "Untuk membatasi inovasi teknologi", correct: false },
-        ],
-    },
-
-
-    {
-        question: "Apa peran hukum dalam mengatur keamanan produk informatika?", //15//
-        answers: [
-            { text: "Memperketat kontrol atas konten internet", correct: false },
-            { text: "Menetapkan standar untuk perlindungan data dan privasi", correct: true },
-            { text: "Meningkatkan biaya produksi perangkat lunak", correct: false },
-            { text: "Mengurangi inovasi dalam teknologi digital", correct: false },
-            { text: "Meniadakan akses internet di wilayah tertentu", correct: false },
-        ],
-    },
-];
 
 let currentQuestionIndex = 0;
 let userScore = 0;
@@ -272,9 +213,8 @@ function resetContainer() {
 
 // Passwords list
 const passwords = [
-    { first: "s", second: "5" },
-    { first: "m", second: "10" },
-    { first: "d", second: "15" },
+    { first: "sdx", second: "5" },
+   
     // Add more passwords as needed
 ];
 
